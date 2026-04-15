d3757860e5a61721e4dc6d847265295981a5ad70
tquanhihi <emlangoailecuaanh@gmail.com>
2026-04-15 21:20:02 +0700

    .

--- src/main/java/geoinfo/server/handler/ClientHandler.java
+++ src/main/java/geoinfo/server/handler/ClientHandler.java
@@ -12,32 +12,6 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class ClientHandler {
-    //    public static void handleClient(Socket socket) {
-//        System.out.println("Server received connection from Client UI: " + socket.getRemoteSocketAddress());
-//
-//        try (
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-//                PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8), true)
-//        ) {
-//            String dataFromClient;
-//            while ((dataFromClient = reader.readLine()) != null) {
-//                if (dataFromClient.trim().equalsIgnoreCase("bye")) {
-//                    writer.println(new JSONObject()
-//                            .put("status", "Success")
-//                            .put("message", "End of connection, goodbye!")
-//                            .toString(2));
-//                    writer.println("<END>");
-//                    break;
-//                }
-//
-//                String responseData = DataProcessor.processData(dataFromClient);
-//                writer.println(responseData);
-//                writer.println("<END>");
-//            }
-//        } catch (IOException e) {
-//            System.out.println("Disconnected Client: " + e.getMessage());
-//        }
-//    }
     public static void handleClient(Socket socket) {
         try (
                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
@@ -59,7 +33,6 @@ public class ClientHandler {
                     break;
                 }
 
-                // phan hoi ma hoa
                 String responseData = DataProcessor.processData(decryptedInput);
                 writer.println(SecurityUtils.encryptAES(responseData, aesKey));
                 writer.println("<END>");
